a66708c56bbb76d026af369d70ebaafa62f5f6e4
MuneebKhalsai <muneeb.akhlaq05@gmail.com>
2025-05-18 00:39:27 +0500

    first commit

---
+++ app/contact/page.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+const Contact = () => {
+    return(
+        <>
+        <h1 className="text-center">Contact</h1>
+        
+        </>
+    )
+}
+export default Contact
--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
 import React from "react"
 const Home = () => {
+  return(
   <>
+
   <h1 className="text-center">Home</h1>
+
+  <h2>Find Github</h2>
+        <input className="1px solid black" type="text" placeholder="Enter User Name" />
+        <button>Submit</button>
   </>
+  )
 }
 export default Home
